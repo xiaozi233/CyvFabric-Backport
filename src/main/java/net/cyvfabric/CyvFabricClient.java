d5e8d2e4f92d472d5c441ddb946747a3dbe92713
Morpheye <morpheyespambot@gmail.com>
2024-02-27 21:32:21 +1100

    initial commit

---
+++ src/main/java/net/cyvfabric/CyvFabricClient.java
@@ -0,0 +1,10 @@
+package net.cyvfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CyvFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cyvfabric/CyvFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cyvfabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CyvFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
